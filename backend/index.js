0de10a7a62e67614c161f75673cd80e1c2fbbc61
zimple kabariya <zkabari1@binghamton.edu>
2020-07-14 22:55:42 -0400

    added folder structures and express web server

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get('/',(req,res) =>
+    res.send(`Our Soccer app is running on port ${PORT}`)
+);
+
+app.listen(PORT, () =>
+    console.log(`Your soccer server is running on port ${PORT}`)
+);
